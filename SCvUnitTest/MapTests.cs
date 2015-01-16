66f52a9b5b27e04d23a017f57874f30e0189e598
Nicolas Hurman <nicolas@nhurman.fr>
2015-01-16 23:50:58 +0100

    Final version

--- SCvUnitTest/MapTests.cs
+++ SCvUnitTest/MapTests.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SCvLib;
 
 namespace SCvUnitTest
 {
-    class MapTests
+    [TestClass]
+    public class MapTests
     {
+        [TestMethod]
+        public void TestDistance()
+        {
+        }
+        [TestMethod]
+        public void TestMoveCost()
+        {
+        }
     }
 }
