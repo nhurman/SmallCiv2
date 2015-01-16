0aab9406eb9dd781b1bf8dd804807b81513b8ece
Alexandre Leonardi <alexandre.leonardi@free.fr>
2015-01-16 23:18:19 +0100

    création tests unitaires

---
+++ SCvUnitTest/GameBuilderTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SCvLib;
+
+namespace SCvUnitTest
+{
+    [TestClass]
+    public class GameBuilderTests
+    {
+        [TestMethod]
+        void TestNew(){
+            Player p1 = new Player();
+            p1.Faction = FactionType.Elves;
+            p1.Name = "A";
+            p1.Score = 0;
+            Player p2 = new Player();
+            p1.Faction = FactionType.Dwarves;
+            p1.Name = "B";
+            p1.Score = 0;
+            Player p3 = new Player();
+            p1.Faction = FactionType.Orcs;
+            p1.Name = "C";
+            p1.Score = 0;
+            Game g = GameBuilder.New(MapType.Demo, p1, p2);
+
+            Assert.AreEqual(g.Map.Width, 6);
+            Assert.AreEqual(g.Map.Turns, 5);
+            Assert.AreEqual(g.Map.Units, 4);
+            Assert.AreEqual(g.Player1, p1);
+            Assert.AreEqual(g.Player2, p2);
+            
+        }
+
+        /*[TestMethod]
+        void TestLoad(){
+
+        }*/
+
+        /*[TestMethod]
+        void TestSave()
+        {
+
+        }*/
+    }
+}
---
+++ SCvUnitTest/GameTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SCvLib;
+
+namespace SCvUnitTest
+{
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void TestGameCreation()
+        {
+            Game game = new Game();
+            Assert.AreEqual(game.Turn, 0);
+            Assert.AreEqual(game.CurrentPlayerId, 0);
+            Assert.AreEqual(Game.Instance, game);
+        }
+       
+        /*[TestMethod]
+        public void TestNextTurn()
+        {
+            Game game = new Game();
+            game.NextTurn();
+            Assert.AreEqual(game.CurrentPlayerId, 1);
+            Assert.AreEqual(game.Turn,0);
+            game.NextTurn();
+            Assert.AreEqual(game.CurrentPlayerId, 1);
+            Assert.AreEqual(game.Turn, 1);
+        }*/
+        
+       /* [TestMethod]
+        public void TestCalculateUnitStats()
+        {
+            Game game = new Game();
+        }*/
+
+        /*[TestMethod]
+        public void TestIsEnded()
+        {
+
+        }*/
+    }
+}
